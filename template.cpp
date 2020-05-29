b33f09deca055d8b95056ce78c720ebcf1ce440f
Muhammad Al-Barham <mohammedbrham98@gmail.com>
2020-05-29 16:57:59 +0300

    Add templte verstoin 1

---
+++ template.cpp
@@ -0,0 +1,30 @@
+//Michał Glapa
+#include<vector>
+#include<algorithm>
+#include<queue>
+#include<map>
+#include<cstdlib>
+#include<string>
+#include<stack>
+#include<cmath>
+#include<iostream>
+
+using namespace std;
+
+#define FOR(i,j,k) for(int i=j;i<k;i++)
+#define FORD(i,j,k) for(int i=j;i>=k;i--)
+#define ll long long
+
+#define ld long double
+const int INF = 1000000009;
+const ld EPS = 10e-9;
+
+
+int main() {
+
+
+
+
+
+    return 0 ; 
+}
